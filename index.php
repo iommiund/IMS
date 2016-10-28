5aded178d067098dc9b74ad99ba14a45a81440ea
Iommi Underwood <iommiund@gmail.com>
2016-10-28 23:52:48 +0200

    .idea files

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<div class="middle">
 			<div class="inner">
 				<div class="login-card">
+					<h1><strong>IMS</strong>login</h1><br>
 					<form action="main.php?login" method="post">
 						<input type="text" name="username" placeholder="Username">
 						<input type="password" name="password" placeholder="Password">
