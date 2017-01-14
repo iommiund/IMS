fbf8b7accdfde61624e1155101df49a4bd8bcef0
Iommi Underwood <iommiund@gmail.com>
2017-01-14 18:25:55 +0100

    .

--- Setup/Config/config.php
+++ Setup/Config/config.php
@@ -1,10 +1,16 @@
 <?php
-	//defining variables
-	define('DB_HOST','localhost');
-	define('DB_USER','iommi');
-	define('DB_PASS','P@$$w0rd87');
-	define('DB_NAME','itsrms_iommiunderwood'); 
-	
-	//connection to mysql server
-	$conn=mysql_connect(DB_HOST, DB_USER, DB_PASS) or die ("Error connecting to server: ".mysql_error());
+//defining variables
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "root";
+$dbname = "ims_iommiunderwood";
+
+//connection to mysql server
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+//test connection
+if (mysqli_connect_errno()) {
+    die ("Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+}
+
 ?>
--- Setup/install.php
+++ Setup/install.php
@@ -1,29 +1,29 @@
 <?php
 
-	include_once ('/Config/config.php');
-	
-	echo "starting install...<br><br>";
-	
-	//insert dbdump in the same folder as install.php (put them in a separate folder)
-	if (empty(DB_PASS)) {
-	$command = "C:\\xampp\\mysql\\bin\\mysql -u".DB_USER." < itsrms_iommiunderwood.sql";
-	
-	exec($command, $output, $return_var);
-	
-	$output = shell_exec($command);
-		
-	echo "database created";
-	
-	} else {
-	
-	$command = "C:\\xampp\\mysql\\bin\\mysql -u".DB_USER." -p".DB_PASS." < itsrms_iommiunderwood.sql";
-	
-	exec($command, $output, $return_var);
-	
-	$output = shell_exec($command);
-		
-	echo "database created";
-	
-	}
+include_once('/Config/config.php');
+
+echo "starting install...<br><br>";
+
+//insert dbdump in the same folder as install.php (put them in a separate folder)
+if (empty($dbpass)) {
+    $command = "C:\\xampp\\mysql\\bin\\mysql -u" . $dbuser . " < ims_iommiunderwood.sql";
+
+    exec($command, $output, $return_var);
+
+    $output = shell_exec($command);
+
+    echo "database created";
+
+} else {
+
+    $command = "C:\\xampp\\mysql\\bin\\mysql -u" . $dbuser . " -p" . $dbpass . " < ims_iommiunderwood.sql";
+
+    exec($command, $output, $return_var);
+
+    $output = shell_exec($command);
+
+    echo "database created";
+
+}
 
 ?>
