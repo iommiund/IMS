e0295eaceb70a5b81412d123e185ac024f6c4c6f
Iommi Underwood <iommiund@gmail.com>
2017-03-03 18:31:13 +0100

    .

--- Android/getOrders.php
+++ Android/getOrders.php
@@ -1,7 +1,28 @@
 <?php
-include_once "../Setup/Config/config.php";
 
-$query=mysqli_query($con,"SELECT 
+//get page number to be displayed
+$page = $_GET['page'];
+
+$start = 0;
+
+$limit = 3;
+
+require_once "../Setup/Config/config.php";
+
+//get total number of orders
+$total = mysqli_num_rows(mysqli_query($con,"SELECT o.order_id FROM ims.orders o"));
+
+//setting page limit
+$page_limit = $total/$limit;
+
+//if page number > than page limit no data to display
+if($page<=$page_limit){
+
+    //calculate start for each page
+    $start = ($page - 1) * $limit;
+
+    //get orders data in range
+    $sql = "SELECT 
                 o.order_id,
                 ca.customer_account_id,
                 CONCAT(ca.customer_name,
@@ -29,8 +50,36 @@ $query=mysqli_query($con,"SELECT
                 ims.users u ON o.initiation_uid = u.uid
             WHERE
                 o.order_status_id = 2
-            ORDER BY 1");
+            ORDER BY 1
+            LIMIT $start, $limit";
 
+    //get results
+    $results = mysqli_query($con,$sql);
+
+    //add results to array
+    $res = array();
+
+    while($row = mysqli_fetch_array($results)){
+        array_push($res, array(
+            "Order Id" => $row['order_id'],
+            "Customer Account Id" => $row['customer_account_id'],
+            "Customer Name" => $row['customerName'],
+            "Address" => $row['address'],
+            "Transaction Type" => $row['transaction_type'],
+            "Old Resource" => $row['old_resource'],
+            "Resource Type" => $row['resource_type'],
+            "Initiation Timestamp" => $row['initiation_timestamp'],
+            "Username" => $row['username']
+        ));
+    }
+
+    //display result in json format
+    echo json_encode($res);
+
+} else {
+    echo "No results to show";
+}
+/*
 if ($query) {
     while($row=mysqli_fetch_array($query)){
         $flag[] = $row;
@@ -39,4 +88,4 @@ if ($query) {
     print(json_encode($flag));
 }
 
-mysqli_close($con);
+mysqli_close($con);*/
